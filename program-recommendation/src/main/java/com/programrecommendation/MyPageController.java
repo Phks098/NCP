6b7e48b01ca56993ea3d90735089a958b345c1fd
Jusung_Park <juke2391@gmail.com>
2022-09-14 03:01:44 +0900

    [refresh]: mypage UI 추가

--- program-recommendation/src/main/java/com/programrecommendation/MyPageController.java
+++ program-recommendation/src/main/java/com/programrecommendation/MyPageController.java
@@ -4,12 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class indexController {
-	
-	@GetMapping("/index")
-	public String index() {
-		return "index";
+public class MyPageController {
+	@GetMapping("/mypage")
+	public String mypage() {
+		return "mypage";
 	}
-	
-	
 }
