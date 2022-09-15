396cc24cc8a1b535550484b1964a6db5d52f0cc2
Jusung_Park <juke2391@gmail.com>
2022-09-15 20:44:48 +0900

    [refresh]: 교수모드 ui 생성"
    ;

---
+++ program-recommendation/src/main/java/com/programrecommendation/ProfessorMode.java
@@ -0,0 +1,12 @@
+package com.programrecommendation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfessorMode {
+	@GetMapping("/professor")
+	public String professor() {
+		return "professor_search_student";
+	}
+}
